a2fe918ee19f35fb188124c87eba260ebf3e981e
Luis Guerra <luis910gg@gmail.com>
2026-03-30 13:25:28 -0400

    feat(app): initialize visor_materias project with React + Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  let aproveCredits = 0
+  let totalCredits = 0
+  let aproveSubjects = 0
+  let totalSubjects = 0
+
+  return (
+    <>
+      <h1>Titutlo</h1>
+      <h2>Creditos aprobados: {aproveCredits} / {totalCredits} </h2>
+      <h2>Materias aprobadas: {aproveSubjects} / {totalSubjects} </h2>
+
+      <div>
+        ...
+      </div>
+
+    </>
+  )
+}
+
+export default App
