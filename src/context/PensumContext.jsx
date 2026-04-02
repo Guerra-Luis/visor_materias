9e247fae4242c3701cebb4ab76262500b336d849
Luis Guerra <luis910gg@gmail.com>
2026-04-02 11:33:37 -0400

    feat(pensum): add subject requirement validation logic

--- src/context/PensumContext.jsx
+++ src/context/PensumContext.jsx
@@ -33,15 +33,60 @@ export function PensumProvider({ children, pensumData }) {
     return totalCredits
   }
 
+  const isRequirementMet = (requirement) => {
+    if (!requirement) return true
+
+    // Requisito de creditos aprovados
+    const ucaMatch = requirement.match(/^(\d+)\s*U\.C\.A\.$/)
+    if (ucaMatch) {
+      const requiredCredits = parseInt(ucaMatch[1])
+      return getApprovedCredits() >= requiredCredits
+    }
+
+    // Requisito de materia paralela
+    const parallelMatch = requirement.match(/^P\((\w+)\)$/)
+    if (parallelMatch) {
+      const subjectCode = parallelMatch[1]
+
+      return (subjectStates[subjectCode] === SUBJECT_STATES.PASSED) ||
+        (subjectStates[subjectCode] === SUBJECT_STATES.IN_PROGRESS)
+    }
+
+    if (subjectsByCode[requirement]) {
+      return subjectStates[requirement] === SUBJECT_STATES.PASSED
+    }
+
+    else return false
+  }
+
+  const areRequirementMet = (subjectCode) => {
+    const subject = subjectsByCode[subjectCode]
+    if (!subject['requisitos'] || !subject) return true
+    return subject['requisitos'].every(req => isRequirementMet(req))
+  }
+
   const getSubjectState = (subjectCode) => {
+    if (!subjectStates[subjectCode]) {
+      if (!subjectCode) return SUBJECT_STATES.UNAVAILABLE //Esto es testeo
+      if (areRequirementMet(subjectCode)) {
+        return SUBJECT_STATES.AVAILABLE
+      }
+      return SUBJECT_STATES.UNAVAILABLE
+    }
     return subjectStates[subjectCode] || 'unavailable'
   }
 
   const setSubjectState = (subjectCode, newState) => {
+    if (newState === SUBJECT_STATES.AVAILABLE &&
+      !areRequirementMet(subjectCode)) {
+      return
+    }
+
     setSubjectStates(prev => ({
       ...prev,
       [subjectCode]: newState
     }))
+
   }
 
   const value = {
