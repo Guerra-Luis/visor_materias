fcf4300c4a6f39254bb4854cca38ff77b6fd812a
Luis Guerra <luis910gg@gmail.com>
2026-04-03 16:00:34 -0400

    refactor(ui): tidied subject and semester component formatting and styles

--- src/components/Semester.jsx
+++ src/components/Semester.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import { Subject } from './Subject'
+import React from "react";
+import { Subject } from "./Subject";
 
 export function Semester({ semesterData }) {
-  const semesterText = semesterData[0]
-  const subjects = semesterData[1]['materias']
+  const semesterText = semesterData[0];
+  const subjects = semesterData[1]["materias"];
   return (
-    <div className=''>
-      <h3>
-        {semesterText.replace('_', ' ').toUpperCase()}
-      </h3>
-      <div className='flex flex-wrap gap-4'>
+    <div>
+      <h3 className="my-8">{semesterText.replace("_", " ").toUpperCase()}</h3>
+      <div className="flex flex-wrap gap-4 justify-center">
         {subjects.map((subject, index) => (
           <Subject subjectData={subject} key={index} />
         ))}
       </div>
     </div>
-  )
+  );
 }
--- src/components/Subject.jsx
+++ src/components/Subject.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import { usePensum } from '../context/PensumContext'
-import { SUBJECT_STATES, STATE_STYLES } from '../constants/subjectStates'
-
+import React from "react";
+import { usePensum } from "../context/PensumContext";
+import { SUBJECT_STATES, STATE_STYLES } from "../constants/subjectStates";
 
 export function Subject({ subjectData }) {
-  const { getSubjectState, setSubjectState } = usePensum()
+  const { getSubjectState, setSubjectState } = usePensum();
 
-  const currentState = getSubjectState(subjectData['codigo'])
-  const styles = STATE_STYLES[currentState]
+  const currentState = getSubjectState(subjectData["codigo"]);
+  const styles = STATE_STYLES[currentState];
 
   const handleClick = () => {
-    const stateOrder = ['available', 'in_progress', 'passed']
-    const currentIndex = stateOrder.indexOf(currentState)
-    const nextState = stateOrder[(currentIndex + 1) % stateOrder.length]
-    setSubjectState(subjectData['codigo'], nextState)
-  }
+    const stateOrder = ["available", "in_progress", "passed"];
+    const currentIndex = stateOrder.indexOf(currentState);
+    const nextState = stateOrder[(currentIndex + 1) % stateOrder.length];
+    setSubjectState(subjectData["codigo"], nextState);
+  };
 
   return (
     <div
-      className={
-        `rounded-lg w-64 p-4 border-2 cursor-pointer inline-block 
+      className={`rounded-lg w-64 p-3 border-2 cursor-pointer inline-block 
         ${styles.bg} ${styles.border} ${styles.text}
       `}
       onClick={handleClick}
     >
-      <div className='flex justify-between'>
-        <span>{subjectData['codigo']}</span>
-        <span>{subjectData['UC']} UC</span>
-      </div>
-      <div className='inline-block'>{subjectData['nombre']}</div>
+      <span className="text-sm text-gray-500">{subjectData["UC"]} UC</span>
+      <span className="h-[3em] my-2 flex items-center justify-center font-semibold">
+        {subjectData["nombre"]
+          .replace("LABORATORIO DE ", "LAB. ")
+          .replace("PROBLEMÁTICA ", "PROB. ")}
+      </span>
+      <span className="text-sm text-gray-500">({subjectData["codigo"]})</span>
     </div>
-  )
+  );
 }
